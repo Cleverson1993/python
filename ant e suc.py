6834d3db4eed62912d087cfb6a6269169a7bc79a
Cleverson1993 <163061546+Cleverson1993@users.noreply.github.com>
2024-09-14 18:35:01 -0300

    tipo de primitivo

---
+++ ant e suc.py
@@ -0,0 +1,4 @@
+n=int(input('Digite um número.'))
+ant=n-1
+suc=n+1
+print('O antecessor do {} é {}, e o sucessor é : {}'. format(n,ant,suc))
---
+++ operadores aritméticos.py
@@ -0,0 +1,10 @@
+n1=int(input('Digite um valor :'))
+n2=int(input('Digite outro valor :'))
+s=n1+n2
+m=n1*n2
+d= n1/n2
+di=n1//n2
+e=n1**n2
+ss=n1-n2
+print('A soma é: {}, o produto é:  {}, e a divisão é: {}'.format(s,m,d))
+print('A divisão inteira é: {}, a potência é: {}, e a subtração é: {}'.format(di,e,ss))
