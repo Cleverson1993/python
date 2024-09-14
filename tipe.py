9bae43e0870ae8a5c1383e19f1f8e56e26be3e61
Cleverson1993 <163061546+Cleverson1993@users.noreply.github.com>
2024-09-14 17:41:39 -0300

    tipo de primitivo

--- tipe.py
+++ tipe.py
@@ -1,9 +1,10 @@
 
 
 a=input('digite algo')
-print(type(a))
+print('O tipo primitivo é' , type(a))
 print(a.isnumeric())
 print(a.isalpha())
 print(a.isspace())
 print(a.istitle())
 print(a.isupper())
+
