ee00142b67dd1459314c54b2298d342f3df55feb
Cleverson1993 <163061546+Cleverson1993@users.noreply.github.com>
2024-09-15 13:31:31 -0300

    tipo de primitivo

---
+++ d,t,r..py
@@ -0,0 +1,11 @@
+numero=float(input('Digite um número :'))
+#Calcula o dobro ,o triplo e a raiz quadrada do número.
+dobro = numero*2
+triplo = numero*3
+raiz_quadrada= numero**0.5
+#Exibe os resultados
+print('O dobro do {} é: {}'.format(numero,dobro))
+print('O triplo do {} é: {}'.format(numero,triplo))
+print('A raiz quadrada do {} é: {}'.format(numero,raiz_quadrada))
+
+
