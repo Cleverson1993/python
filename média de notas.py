850f5774e55dde4b3d5540002e10257685962355
Cleverson1993 <163061546+Cleverson1993@users.noreply.github.com>
2024-09-15 14:38:05 -0300

    média das notas

---
+++ média de notas.py
@@ -0,0 +1,7 @@
+# solicita ao usuário que insira as notas
+nota1=float(input('Digite a primeira nota:'))
+nota2=float(input('Digite a segunda nota'))
+#Calcula as médias da nota
+media=(nota1+nota2)/2
+#Exibe a média das notas
+print('A média das notas é: {}'.format({media}))
