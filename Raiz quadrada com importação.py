f987a6b424e430dd2315fc8e3a238cc2cf7dc189
Cleverson1993 <163061546+Cleverson1993@users.noreply.github.com>
2024-09-27 19:45:38 -0300

    Raiz quadrada com importação

---
+++ Raiz quadrada com importação.py
@@ -0,0 +1,9 @@
+import math
+# Solicite um número ao usuário
+num = int ( input(' Digite um número inteiro :'))
+
+# Calcula a raiz quadrada
+raiz = math.sqrt (num)
+
+# exibe o resultado
+print(f' A raiz quadrada de {num} é : {raiz}')
