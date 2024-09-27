24fb70a182909fb76a2b2c940f98cb824dbfad11
Cleverson1993 <163061546+Cleverson1993@users.noreply.github.com>
2024-09-27 18:07:08 -0300

    conversor de temperatura

---
+++ conversor de temperatura.py
@@ -0,0 +1,8 @@
+# Solicita que o usuário informe a temperatura
+c = float(input('Informe a temperatura em ºC : ' ))
+
+# Faz a conversão
+f= ((9*c)/5) + 32
+
+# Mostra a temperatura convertida
+print(f'A temperatura{c}ºC corresponde a {f} ºF !')
