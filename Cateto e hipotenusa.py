b93e738d406e9cf3cf51617950da2e3b2f52b54f
Cleverson1993 <163061546+Cleverson1993@users.noreply.github.com>
2024-09-28 15:08:27 -0300

    cateto e hipotenuza

---
+++ Cateto e hipotenusa.py
@@ -0,0 +1,4 @@
+co = float(input(' comprimento do cateto oposto :'))
+ca = float(input(' comprimento do cateto adjacente :'))
+hi = (co ** 2 + ca ** 2) ** (1/2)
+print(f' A hipotenusa vai medir : {hi}')
